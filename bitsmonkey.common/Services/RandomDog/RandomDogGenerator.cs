20e5c5dd4cde95a288020d63487c3666e74cfb2b
unrealnerd <arjunshetty2020@gmail.com>
2019-12-22 21:21:06 +0530

    * addig breed to tile of image comp

--- bitsmonkey.common/Services/RandomDog/RandomDogGenerator.cs
+++ bitsmonkey.common/Services/RandomDog/RandomDogGenerator.cs
@@ -2,6 +2,7 @@ using System.Net.Http;
 using System.Runtime.Serialization;
 using System.Runtime.Serialization.Json;
 using System.Threading.Tasks;
+using System.Linq;
 
 namespace bitsmonkey.common.Services.CorporateBuzzWords
 {
@@ -36,11 +37,20 @@ namespace bitsmonkey.common.Services.CorporateBuzzWords
 
                 randomDogGeneratorResponse = new DataContractJsonSerializer(typeof(RandomDogGeneratorResponse)).ReadObject(await randomDogStream) as RandomDogGeneratorResponse;
             }
+
             return new
             {
                 ImageUrl = randomDogGeneratorResponse?.ImgUrl,
+                Title = ExtractBreedFromUrl(randomDogGeneratorResponse?.ImgUrl),
                 Template = Constant.Template.IMAGE
             };
+        }
+
+        private string ExtractBreedFromUrl(string url)
+        {
+            var splitUrl = url.Split('/');
+
+            return splitUrl?.GetValue(splitUrl.GetLength(0) - 2)?.ToString();
         }
     }
 
