e9e7c4e06f6607d4cd5a34bd3bd3615f7b5560aa
unrealnerd <arjunshetty2020@gmail.com>
2020-08-30 22:50:58 +0530

    * naming the netowrk in docker compose * disabling https redirection temporarily * renaming the container settings file

--- bitsmonkey.api/Startup.cs
+++ bitsmonkey.api/Startup.cs
@@ -70,7 +70,7 @@ namespace bitsmonkey.api
             {
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
-                app.UseHttpsRedirection();
+                // app.UseHttpsRedirection();
             }
 
             var resourceFolderPath = Path.Combine(env.ContentRootPath, "Resources");
